531dd3458d5852a6c1e1bf6961fe21c6263fcf94
phatdroid94 <phatdroid94@gmail.com>
2019-12-18 21:38:12 +0700

    finish  setup

--- app/src/main/java/com/example/apptodolistretrofit04092019/api/ApiResponse.java
+++ app/src/main/java/com/example/apptodolistretrofit04092019/api/ApiResponse.java
@@ -1,10 +1,15 @@
 package com.example.apptodolistretrofit04092019.api;
 
+import com.example.apptodolistretrofit04092019.model.Response;
+import com.example.apptodolistretrofit04092019.model.Sinhvien;
+
+import java.util.List;
+
 import io.reactivex.Observable;
 import retrofit2.http.GET;
 
 public interface ApiResponse {
 
     @GET("sinhvien.php")
-    Observable<>
+    Observable<Response<List<Sinhvien>>> getAllSinhVien();
 }
--- app/src/main/java/com/example/apptodolistretrofit04092019/view/activty/MainActivity.java
+++ app/src/main/java/com/example/apptodolistretrofit04092019/view/activty/MainActivity.java
@@ -3,8 +3,19 @@ package com.example.apptodolistretrofit04092019.view.activty;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.apptodolistretrofit04092019.R;
+import com.example.apptodolistretrofit04092019.api.RetrofitInit;
+import com.example.apptodolistretrofit04092019.model.Response;
+import com.example.apptodolistretrofit04092019.model.Sinhvien;
+
+import java.util.List;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +24,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RetrofitInit
+                .getInStance()
+                .getAllSinhVien()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<Response<List<Sinhvien>>>() {
+                    @Override
+                    public void accept(Response<List<Sinhvien>> listResponse) throws Exception {
+                        List<Sinhvien> arraySinhvien = listResponse.getData();
+                        Log.d("BBB",arraySinhvien.size() + "");
+                    }
+                });
     }
 }
